a6828a5d215413c7a9db6f019e90be3e379ed90a
goncalo-frade-iohk <goncalo.frade@iohk.io>
2023-08-27 17:56:10 +0100

    feat(swift): add swift package 0.1.0 release

--- Package.swift
+++ Package.swift
@@ -18,15 +18,22 @@ let package = Package(
         .target(
             name: "AnoncredsSwift",
             dependencies: ["anoncredsFFI"],
-            path: "AnoncredsSwift/Sources/Swift"
+            path: "uniffi/output-frameworks/anoncreds-swift/AnoncredsSwift/Sources/Swift"
         ),
         .target(
             name: "anoncredsFFI",
             dependencies: ["libanoncreds"],
-            path: "AnoncredsSwift/Sources/C"),
+            path: "uniffi/output-frameworks/anoncreds-swift/AnoncredsSwift/Sources/C"),
+        // LOCAL
+//        .binaryTarget(
+//            name: "libanoncreds",
+//            path: "./uniffi/output-frameworks/anoncreds-swift/libanoncreds.xcframework.zip"
+//        )
+        // RELEASE
         .binaryTarget(
             name: "libanoncreds",
-            path: "./libanoncreds.xcframework.zip"
+            url: "https://github.com/input-output-hk/anoncreds-rs/releases/download/0.1.0/libanoncreds.xcframework.zip",
+            checksum: "1a137728ea51214a0159cc05f6820f199ab6d9f9c0be7f8685af69ed65e07b9a"
         )
     ]
 )
